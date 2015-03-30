54c6fd9beafadfdc2395a1012e3ad6f41a33d5eb
Darin Morrison <darinmorrison+git@gmail.com>
2015-03-29 22:45:15 -0600

    Implement generic BitOps trait; fix #2

--- src/generic.rs
+++ src/generic.rs
@@ -1,242 +1,254 @@
-/// Logical and not
-#[inline(always)]
-pub fn b_and_not(src1: u64, src2: u64) -> u64 {
-    !src1 & src2
-}
+use std::{num, ops};
 
-/// Get bit block
-#[inline(always)]
-pub fn b_get_block_from(src: u64, start: u32, len: u32) -> u64 {
-    (src >> start) & ((1 << len) - 1)
-}
-
-/// Get bits included in blocks k > 1 count bits of the same parity
-#[inline(always)]
-pub fn b_get_block(src: u64) -> u64 {
-    !b_get_singletons(src)
-}
-
-/// Gather bits
-#[inline(always)]
-pub fn b_gather(val: u64, mut mask: u64) -> u64 {
-    let mut res = 0u64;
-    let mut bb  = 1u64;
-    while  mask                 != 0 {
-        if  val &  i_get (mask) != 0 {
-            res |= bb;
-        }
-           mask  = i_flip(mask);
-             bb += bb;
+pub trait BitOps: num::Int {
+    /// Logical and not
+    #[inline(always)]
+    fn b_and_not(self, rhs: Self) -> Self {
+        !self & rhs
     }
-    res
-}
 
-/// Return 0s and 1s on block transitions within outer 0s
-#[inline(always)]
-pub fn b_get_border(src: u64) -> u64 {
-    (src ^ (src << 1)) & (src ^ (src >> 1))
-}
-
-/// Get bits bounded on the left and right by bits of the same parity
-#[inline(always)]
-pub fn b_get_interior(src: u64) -> u64 {
-    !b_get_border(src)
-}
-
-/// Get singleton 0s and 1s within outer 0s
-#[inline(always)]
-pub fn b_get_singletons(src: u64) -> u64 {
-    (src ^ (src << 1)) & (src ^ (src >> 1))
-}
-
-/// Get singleton 0s and 1s
-#[inline(always)]
-pub fn b_get_singletons_xi(src: u64) -> u64 {
-    i_get_singletons_xi(src) | o_get_singletons_xi(src)
-}
-
-/// Scatter bits
-#[inline(always)]
-pub fn b_scatter(val: u64, mut mask: u64) -> u64 {
-    let mut res = 0u64;
-    let mut bb  = 1u64;
-    while  mask       != 0 {
-        if  val &  bb != 0 {
-            res |= i_get (mask);
-        }
-           mask  = i_flip(mask);
-             bb += bb;
+    /// Get bit block
+    #[inline(always)]
+    fn b_get_block_from(self, start: usize, len: usize) -> Self {
+        (self >> start) & ((Self::one() << len) - Self::one())
     }
-    res
+
+    /// Get bits included in blocks k > 1 count bits of the same parity
+    #[inline(always)]
+    fn b_get_block(self) -> Self {
+        !self.b_get_singletons()
+    }
+
+    /// Gather bits
+    #[inline(always)]
+    fn b_gather(self, mut mask: Self) -> Self where Self: ops::Neg<Output = Self> {
+        let mut res = Self::zero();
+        let mut bin = Self::one ();
+        while  mask                 != Self::zero() {
+            if self & mask.i_get () != Self::zero() {
+                res = res | bin;
+            }
+               mask = mask.i_flip();
+                bin = bin + bin;
+        }
+        res
+    }
+
+    /// Return 0s and 1s on block transitions within outer 0s
+    #[inline(always)]
+    fn b_get_border(self) -> Self {
+        (self ^ (self << 1)) & (self ^ (self >> 1))
+    }
+
+    /// Get bits bounded on the left and right by bits of the same parity
+    #[inline(always)]
+    fn b_get_interior(self) -> Self {
+        !self.b_get_border()
+    }
+
+    /// Get singleton 0s and 1s within outer 0s
+    #[inline(always)]
+    fn b_get_singletons(self) -> Self {
+        (self ^ (self << 1)) & (self ^ (self >> 1))
+    }
+
+    /// Get singleton 0s and 1s
+    #[inline(always)]
+    fn b_get_singletons_xi(self) -> Self {
+        self.i_get_singletons_xi() | self.o_get_singletons_xi()
+    }
+
+    /// Scatter bits
+    #[inline(always)]
+    fn b_scatter(self, mut mask: Self) -> Self where Self: ops::Neg<Output = Self> {
+        let mut res = Self::zero();
+        let mut bin = Self::one ();
+        while  mask       != Self::zero() {
+            if self & bin != Self::zero() {
+                res = res | mask.i_get ();
+            }
+               mask = mask.i_flip();
+                bin = bin + bin;
+        }
+        res
+    }
+
+    /// Zero high bits from specified position
+    #[inline(always)]
+    fn b_zero_high_from_pos(self, index: usize) -> Self {
+        self & ((Self::one() << index) - Self::one())
+    }
+
+    /// Count total 1s; population count
+    #[inline(always)]
+    fn i_count_total(self) -> u32 {
+        self.count_ones()
+    }
+
+    /// Fill up to lowest 1
+    #[inline(always)]
+    fn i_fill_upto(self) -> Self {
+        self | self.wrapping_sub(Self::one())
+    }
+
+    /// Get lowest 1
+    #[allow(unsigned_negation)]
+    #[inline(always)]
+    fn i_get(self) -> Self where Self: ops::Neg<Output = Self> {
+        self & -self
+    }
+
+    /// Get 1s included in blocks k > 1 count 1s
+    #[inline(always)]
+    fn i_get_block(self) -> Self {
+        self & ((self << 1) | (self >> 1))
+    }
+
+    /// Get 1s bordering blocks of k > 1 count 1s
+    #[inline(always)]
+    fn i_get_block_border(self) -> Self {
+        self & ((self << 1) ^ (self >> 1))
+    }
+
+    /// Get 1s to the left of blocks of k > 1 count 1s
+    #[inline(always)]
+    fn i_get_block_border_low(self) -> Self {
+        let shr = self >> 1;
+        (self & ((self << 1) ^ shr)) & shr
+    }
+
+    /// Get 1s to the right of blocks of k > 1 count 1s
+    #[inline(always)]
+    fn i_get_block_border_high(self) -> Self {
+        let shl = self << 1;
+        (self & (shl ^ (self >> 1))) & shl
+    }
+
+    /// Get 1s bordering 0s
+    #[inline(always)]
+    fn i_get_border(self) -> Self {
+        self & !((self << 1) & (self >> 1))
+    }
+
+    /// Get 1s to the right of 0s
+    #[inline(always)]
+    fn i_get_border_high(self) -> Self {
+        self & (self ^ (self >> 1))
+    }
+
+    /// Get 1s to the left of 0s
+    #[inline(always)]
+    fn i_get_border_low(self) -> Self {
+        self & (self ^ (self << 1))
+    }
+
+    /// Get 1s bounded by 1s on the left and right
+    #[inline(always)]
+    fn i_get_interior(self) -> Self {
+        self & ((self << 1) & (self >> 1))
+    }
+
+    /// Get lowest 1 and complement word
+    #[inline(always)]
+    fn i_get_invert(self) -> Self {
+        !self | self.wrapping_sub(Self::one())
+    }
+
+    /// Get singleton 1s within outer 0s
+    #[inline(always)]
+    fn i_get_singletons(self) -> Self {
+        self & !((self << 1) | (self >> 1))
+    }
+
+    /// Get singleton 1s
+    #[inline(always)]
+    fn i_get_singletons_xi(self) -> Self {
+        self.i_get_singletons()
+    }
+
+    /// Mask trailing 1s and complement word
+    #[inline(always)]
+    fn i_mask_upto_invert(self) -> Self {
+        !self | (self + Self::one())
+    }
+
+    /// Mask up to lowest 1
+    #[inline(always)]
+    fn i_mask_upto(self) -> Self {
+        self ^ self.wrapping_sub(Self::one())
+    }
+
+    /// Set lowest 1 to 0
+    #[inline(always)]
+    fn i_flip(self) -> Self {
+        self & self.wrapping_sub(Self::one())
+    }
+
+    /// Count leading 0s
+    #[inline(always)]
+    fn o_count_leading(self) -> u32 {
+        self.leading_zeros()
+    }
+
+    /// Count total 0s
+    #[inline(always)]
+    fn o_count_total(self) -> u32 {
+        self.count_zeros()
+    }
+
+    /// Count trailing 0s
+    #[inline(always)]
+    fn o_count_trailing(self) -> u32 {
+        self.trailing_zeros()
+    }
+
+    /// Set lowest 0 to 1
+    #[inline(always)]
+    fn o_flip(self) -> Self {
+        self | (self + Self::one())
+    }
+
+    /// Fill up to lowest 0
+    #[inline(always)]
+    fn o_fill_upto(self) -> Self {
+        self & (self + Self::one())
+    }
+
+    /// Get lowest 0
+    #[inline(always)]
+    fn o_get(self) -> Self {
+        self | !(self + Self::one())
+    }
+
+    /// Get lowest 0 and complement word
+    #[inline(always)]
+    fn o_get_invert(self) -> Self {
+        !self & (self + Self::one())
+    }
+
+    /// Get singleton 0s within outer 0s
+    #[inline(always)]
+    fn o_get_singletons(self) -> Self {
+        !self & ((self << 1) & (self >> 1))
+    }
+
+    /// Get singleton 0s
+    #[inline(always)]
+    fn o_get_singletons_xi(self) -> Self {
+        (!self).i_get_singletons()
+    }
+
+    /// Mask up to lowest 0
+    #[inline(always)]
+    fn o_mask_upto(self) -> Self {
+        self ^ (self + Self::one())
+    }
+
+    /// Mask trailing 0s
+    #[inline(always)]
+    fn o_mask_upto_invert(self) -> Self {
+        !self & self.wrapping_sub(Self::one())
+    }
 }
 
-/// Zero high bits from specified position
-#[inline(always)]
-pub fn b_zero_high_from_pos(src: u64, index: u32) -> u64 {
-    src & ((1 << index) - 1)
-}
-
-/// Count total 1s; population count
-#[inline(always)]
-pub fn i_count_total(src: u64) -> u64 { unsafe {
-    ::std::intrinsics::ctpop64(src)
-}}
-
-/// Fill up to lowest 1
-#[inline(always)]
-pub fn i_fill_upto(src: u64) -> u64 {
-    src | src.wrapping_sub(1)
-}
-
-/// Get lowest 1
-#[allow(unsigned_negation)]
-#[inline(always)]
-pub fn i_get(src: u64) -> u64 {
-    src & -src
-}
-
-/// Get 1s included in blocks k > 1 count 1s
-#[inline(always)]
-pub fn i_get_block(src: u64) -> u64 {
-    src & ((src << 1) | (src >> 1))
-}
-
-/// Get 1s bordering blocks of k > 1 count 1s
-#[inline(always)]
-pub fn i_get_block_border(src: u64) -> u64 {
-    src & ((src << 1) ^ (src >> 1))
-}
-
-/// Get 1s to the left of blocks of k > 1 count 1s
-#[inline(always)]
-pub fn i_get_block_border_low(src: u64) -> u64 {
-    let src_shr = src >> 1;
-    (src & ((src << 1) ^ src_shr)) & src_shr
-}
-
-/// Get 1s to the right of blocks of k > 1 count 1s
-#[inline(always)]
-pub fn i_get_block_border_high(src: u64) -> u64 {
-    let src_shl = src << 1;
-    (src & (src_shl ^ (src >> 1))) & src_shl
-}
-
-/// Get 1s bordering 0s
-#[inline(always)]
-pub fn i_get_border(src: u64) -> u64 {
-    src & !((src << 1) & (src >> 1))
-}
-
-/// Get 1s to the right of 0s
-#[inline(always)]
-pub fn i_get_border_high(src: u64) -> u64 {
-    src & (src ^ (src >> 1))
-}
-
-/// Get 1s to the left of 0s
-#[inline(always)]
-pub fn i_get_border_low(src: u64) -> u64 {
-    src & (src ^ (src << 1))
-}
-
-/// Get 1s bounded by 1s on the left and right
-#[inline(always)]
-pub fn i_get_interior(src: u64) -> u64 {
-    src & ((src << 1) & (src >> 1))
-}
-
-/// Get lowest 1 and complement word
-#[inline(always)]
-pub fn i_get_invert(src: u64) -> u64 {
-    !src | src.wrapping_sub(1)
-}
-
-/// Get singleton 1s within outer 0s
-#[inline(always)]
-pub fn i_get_singletons(src: u64) -> u64 {
-    src & !((src << 1) | (src >> 1))
-}
-
-/// Get singleton 1s
-#[inline(always)]
-pub fn i_get_singletons_xi(src: u64) -> u64 {
-    i_get_singletons(src)
-}
-
-/// Mask trailing 1s and complement word
-#[inline(always)]
-pub fn i_mask_upto_invert(src: u64) -> u64 {
-    !src | (src + 1)
-}
-
-/// Mask up to lowest 1
-#[inline(always)]
-pub fn i_mask_upto(src: u64) -> u64 {
-    src ^ src.wrapping_sub(1)
-}
-
-/// Set lowest 1 to 0
-#[inline(always)]
-pub fn i_flip(src: u64) -> u64 {
-    src & src.wrapping_sub(1)
-}
-
-/// Count leading 0s
-#[inline(always)]
-pub fn o_count_leading(src: u64) -> u64 { unsafe {
-    ::std::intrinsics::ctlz64(src)
-}}
-
-/// Count trailing 0s
-#[inline(always)]
-pub fn o_count_trailing(src: u64) -> u64 { unsafe {
-    ::std::intrinsics::cttz64(src)
-}}
-
-/// Set lowest 0 to 1
-#[inline(always)]
-pub fn o_flip(src: u64) -> u64 {
-    src | (src + 1)
-}
-
-/// Fill up to lowest 0
-#[inline(always)]
-pub fn o_fill_upto(src: u64) -> u64 {
-    src & (src + 1)
-}
-
-/// Get lowest 0
-#[inline(always)]
-pub fn o_get(src: u64) -> u64 {
-    src | !(src + 1)
-}
-
-/// Get lowest 0 and complement word
-#[inline(always)]
-pub fn o_get_invert(src: u64) -> u64 {
-    !src & (src + 1)
-}
-
-/// Get singleton 0s within outer 0s
-#[inline(always)]
-pub fn o_get_singletons(src: u64) -> u64 {
-    !src & ((src << 1) & (src >> 1))
-}
-
-/// Get singleton 0s
-#[inline(always)]
-pub fn o_get_singletons_xi(src: u64) -> u64 {
-    i_get_singletons(!src)
-}
-
-/// Mask up to lowest 0
-#[inline(always)]
-pub fn o_mask_upto(src: u64) -> u64 {
-    src ^ (src + 1)
-}
-
-/// Mask trailing 0s
-#[inline(always)]
-pub fn o_mask_upto_invert(src: u64) -> u64 {
-    !src & src.wrapping_sub(1)
-}
+impl<T: num::Int> BitOps for T {}
--- src/x86.rs
+++ src/x86.rs
@@ -1,18 +1,18 @@
 /// ABM: Advanced Bit Manipulation Instruction Set
 pub mod abm {
     pub mod u64 {
-        use generic;
+        use generic::BitOps;
 
         /// Count leading 0s
         #[inline(always)]
         pub fn lzcnt(src: u64) -> u64 {
-            generic::o_count_leading(src)
+            src.o_count_leading() as u64
         }
 
         /// Count total 1s; population count
         #[inline(always)]
         pub fn popcnt(src: u64) -> u64 {
-            generic::i_count_total(src)
+            src.i_count_total() as u64
         }
     }
 }
@@ -20,43 +20,43 @@ pub mod abm {
 /// BMI1: Bit Manipulation Instruction Set #1
 pub mod bmi1 {
     pub mod u64 {
-        use generic;
+        use generic::BitOps;
 
         /// Logical and not
         #[inline(always)]
         pub fn andn(src1: u64, src2: u64) -> u64 {
-            generic::b_and_not(src1, src2)
+            src1.b_and_not(src2)
         }
 
         /// Get bit block
         #[inline(always)]
         pub fn bextr(src: u64, start: u32, len: u32) -> u64 {
-            generic::b_get_block_from(src, start, len)
+            src.b_get_block_from(start as usize, len as usize)
         }
 
         /// Get lowest 1
         #[allow(unsigned_negation)]
         #[inline(always)]
         pub fn blsi(src: u64) -> u64 {
-            generic::i_get(src)
+            src.i_get()
         }
 
         /// Mask up to lowest 1
         #[inline(always)]
         pub fn blsmsk(src: u64) -> u64 {
-            generic::i_mask_upto(src)
+            src.i_mask_upto()
         }
 
         /// Set lowest 1 to 0
         #[inline(always)]
         pub fn blsr(src: u64) -> u64 {
-            generic::i_flip(src)
+            src.i_flip()
         }
 
         /// Count trailing 0s
         #[inline(always)]
         pub fn tzcnt(src: u64) -> u64 {
-            generic::o_count_trailing(src)
+            src.o_count_trailing() as u64
         }
     }
 }
@@ -64,24 +64,24 @@ pub mod bmi1 {
 /// BMI2: Bit Manipulation Instruction Set #2
 pub mod bmi2 {
     pub mod u64 {
-        use generic;
+        use generic::BitOps;
 
         /// Zero high bits from specified position
         #[inline(always)]
         pub fn bzhi(src: u64, index: u32) -> u64 {
-            generic::b_zero_high_from_pos(src, index)
+            src.b_zero_high_from_pos(index as usize)
         }
 
         /// Parallel bits deposit; scatter bits
         #[inline(always)]
         pub fn pdep(val: u64, mask: u64) -> u64 {
-            generic::b_scatter(val, mask)
+            val.b_scatter(mask)
         }
 
         /// Parallel bits extract; gather bits
         #[inline(always)]
         pub fn pext(val: u64, mask: u64) -> u64 {
-            generic::b_gather(val, mask)
+            val.b_gather(mask)
         }
     }
 }
@@ -89,61 +89,60 @@ pub mod bmi2 {
 /// TBM: Trailing Bit Manipulation Instruction Set
 pub mod tbm {
     pub mod u64 {
-        use generic;
+        use generic::BitOps;
 
         /// Fill up to lowest 0
         #[inline(always)]
         pub fn blcfill(src: u64) -> u64 {
-            generic::o_fill_upto(src)
+            src.o_fill_upto()
         }
 
         /// Get lowest 0
         #[inline(always)]
         pub fn blci(src: u64) -> u64 {
-            generic::o_get(src)
+            src.o_get()
         }
 
         /// Get lowest 0 and complement word
         #[inline(always)]
         pub fn blcic(src: u64) -> u64 {
-            generic::o_get_invert(src)
+            src.o_get_invert()
         }
 
         /// Mask up to lowest 0
         #[inline(always)]
         pub fn blcmsk(src: u64) -> u64 {
-            generic::o_mask_upto(src)
+            src.o_mask_upto()
         }
 
         /// Set lowest 0 to 1
         #[inline(always)]
         pub fn blcs(src: u64) -> u64 {
-            generic::o_flip(src)
+            src.o_flip()
         }
 
         /// Fill up to lowest 1
         #[inline(always)]
         pub fn blsfill(src: u64) -> u64 {
-            generic::i_fill_upto(src)
+            src.i_fill_upto()
         }
 
         /// Get lowest 1 and complement word
         #[inline(always)]
         pub fn blsic(src: u64) -> u64 {
-            generic::i_get_invert(src)
+            src.i_get_invert()
         }
 
         /// Mask trailing 1s and complement word
         #[inline(always)]
         pub fn t1mskc(src: u64) -> u64 {
-            generic::i_mask_upto_invert(src)
+            src.i_mask_upto_invert()
         }
 
         /// Mask trailing 0s
         #[inline(always)]
         pub fn tzmsk(src: u64) -> u64 {
-            generic::o_mask_upto_invert(src)
+            src.o_mask_upto_invert()
         }
     }
 }
-
